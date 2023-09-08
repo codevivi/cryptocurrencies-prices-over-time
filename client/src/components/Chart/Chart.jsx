28306233f9e169b0a4c7abdfb374e8199404d261
Vilma <vilmail@protonmail.com>
2023-09-08 23:45:54 +0300

    added forgotten styles after move to parent

--- client/src/components/Chart/Chart.jsx
+++ client/src/components/Chart/Chart.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useRef } from "react";
 import { LineChart } from "@carbon/charts-react";
 import { PriceDataCtx } from "../../context/PriceDataCtx";
 import getChartOptions from "./chartOptions";
+import "@carbon/charts/styles.css";
 
 const Chart = () => {
   const { loadingPriceData, priceData, getPricesErrorMsg } = useContext(PriceDataCtx);
@@ -17,12 +18,14 @@ const Chart = () => {
   }, [loadingPriceData, priceData]);
 
   return (
-    <div className="chart-container" tabIndex={-1} ref={chartRef}>
-      <div className="chart">
-        {getPricesErrorMsg && <p className="error-msg">{getPricesErrorMsg}</p>}
-        <LineChart data={priceData === null ? [] : priceData} options={getChartOptions(priceData)}></LineChart>
+    <>
+      <div className="chart-container" tabIndex={-1} ref={chartRef}>
+        <div className="chart">
+          {getPricesErrorMsg && <p className="error-msg">{getPricesErrorMsg}</p>}
+          <LineChart data={priceData === null ? [] : priceData} options={getChartOptions(priceData)}></LineChart>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Chart;
