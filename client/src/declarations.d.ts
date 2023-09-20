193d811b0f0d4741664c624ea7ae1906b1b228d0
Vilma <vilmail@protonmail.com>
2023-09-20 14:28:46 +0300

    add declarations.d.ts * removes errors for missing @carbon/react types

---
+++ client/src/declarations.d.ts
@@ -0,0 +1,3 @@
+// in @carbon/react some components missing types: https://github.com/carbon-design-system/carbon/issues/12513
+// https://github.com/carbon-design-system/carbon/issues/13091
+declare module "@carbon/react";
