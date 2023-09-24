090d15346f6a0c0455308724f412830eda19b694
Vilma <vilmail@protonmail.com>
2023-09-25 00:48:57 +0300

    Untrack dist/,types packages,config to src

--- server/dist/middlewares/errorOnWrongEndPoint.js
+++
@@ -1,5 +0,0 @@
-import CustomError from "../utils/CustomError.js";
-const errorOnWrongEndPoint = (req, res, next) => {
-    throw new CustomError(404, "failure", "Endpoint not found");
-};
-export default errorOnWrongEndPoint;
--- server/dist/utils/consoleLogUserActions.js
+++
@@ -1,5 +0,0 @@
-function consoleLogUserActions(description, value) {
-    const blue = "\x1b[34m%s\x1b[0m";
-    console.log(blue, `USER ACTION: "${description} ${value}`);
-}
-export default consoleLogUserActions;
